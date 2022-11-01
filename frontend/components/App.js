dde4075b5162ad259428a12d26770ddaccff0a84
Lionel Mbayu <Lionel.Mbayu@gmail.com>
2022-11-01 14:32:43 -0500

    Displayed API error

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,6 +6,7 @@ const URL = 'http://localhost:9000/api/todos'
 export default class App extends React.Component {
   state = {
     todos: [],
+    error: '',
   }
 
   fetchAllTodos = () => {
@@ -14,7 +15,7 @@ export default class App extends React.Component {
         this.setState({ ...this.state, todos: res.data.data })
       })
       .catch(err => {
-        debugger
+        this.setState({ ...this.state, error: err.response.data.message })
       })
   }
 
@@ -27,12 +28,12 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <div id='error'>Error: No error here</div>
+        <div id='error'>Error: {this.state.error}</div>
         <div id="todo">
           <h2>Todos:</h2>
           {
             this.state.todos.map(td => {
-              return <div key={td.id}>{ td.name }</div>
+              return <div key={td.id}>{td.name}</div>
             })
           }
         </div>
